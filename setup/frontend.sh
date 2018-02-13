1da4c00e90db83505df08aebbaf2c701844e1ab0
ivanpaulovich <ivan@100loop.com>
2018-02-13 12:55:03 -0200

    Frontend removed

--- setup/frontend.sh
+++
@@ -1,8 +0,0 @@
-DOCKERPULL=`docker pull ivanpaulovich/acerola:latest-frontend`
-if [[ $DOCKERPULL != *"Status: Image is up to date for"* || $1 == '/f' ]]; then
-	docker stop acerola-frontend
-	docker rm acerola-frontend
-	docker run -p 8001:80 -d \
-		--name acerola-frontend \
-		ivanpaulovich/acerola:latest-frontend
-fi
