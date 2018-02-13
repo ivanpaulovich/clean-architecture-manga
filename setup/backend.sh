50ef14a7eff7347611a8cb5bb4cd073aa85a3732
ivanpaulovich <ivan@100loop.com>
2018-02-13 12:28:26 -0200

    setup

--- setup/backend.sh
+++ setup/backend.sh
@@ -1,11 +1,14 @@
-DOCKERPULL=`docker pull ivanpaulovich/acerola:latest`
+#!/bin/bash
+DOCKERPULL=`docker pull ivanpaulovich/manga:latest`
 if [[ $DOCKERPULL != *"Status: Image is up to date for"* || $1 == '/f' ]]; then
-	docker stop acerola-backend
-	docker rm acerola-backend
-	docker run -p 8000:80 \
-		-e modules__0__properties__ConnectionString=mongodb://172.17.0.1:27017 \
-		-e modules__1__properties__ConnectionString=mongodb://172.17.0.1:27017 \
-		-d \
-		--name acerola-backend \
-		ivanpaulovich/acerola:latest
+        echo "Updating"
+        docker stop manga-backend
+        docker rm manga-backend
+        docker run -p 8000:80 \
+                -e modules__2__properties__ConnectionString=mongodb://172.17.0.1:27017 \
+                -d \
+                --name manga-backend \
+                ivanpaulovich/manga:latest
+else
+        echo "Image is already updated"
 fi
