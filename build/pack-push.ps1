d70d2ae57043623b5e2272cc0755602254272abd
ivanpaulovich <ivan@100loop.com>
2018-07-05 21:55:46 +0200

    nuget

--- build/pack-push.ps1
+++ build/pack-push.ps1
@@ -1,3 +1,3 @@
 nuget.exe pack ../template/Paulovich.Manga.nuspec
-nuget.exe setApiKey $NUGET_API_KEY -Source $NUGET_SOURCE
-nuget.exe push ../template/Paulovich.Manga.*.nupkg -Source $NUGET_SOURCE
+nuget.exe setApiKey $env:NUGET_API_KEY -Source $env:NUGET_SOURCE
+nuget.exe push ../template/Paulovich.Manga.*.nupkg -Source $env:NUGET_SOURCE
