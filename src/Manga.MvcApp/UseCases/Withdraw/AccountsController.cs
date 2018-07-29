0f56ac76f86ba8baf85dedcda7bd344f28cc6329
ivanpaulovich <ivan@100loop.com>
2018-07-29 21:33:53 +0200

    New use cases for #17

---
+++ src/Manga.MvcApp/UseCases/Withdraw/AccountsController.cs
@@ -0,0 +1,43 @@
+﻿namespace Manga.MvcApp.UseCases.Withdraw
+{
+    using Manga.Application.UseCases.Withdraw;
+    using Microsoft.AspNetCore.Mvc;
+    using System.Threading.Tasks;
+
+    public sealed class AccountsController : Controller
+    {
+        private readonly IWithdrawUseCase _withdrawUseCase;
+        private readonly Presenter _presenter;
+
+        public AccountsController(
+            IWithdrawUseCase withdrawUseCase,
+            Presenter presenter)
+        {
+            _withdrawUseCase = withdrawUseCase;
+            _presenter = presenter;
+        }
+
+        public IActionResult Withdraw()
+        {
+            return View();
+        }
+
+        /// <summary>
+        /// Register a new Customer
+        /// </summary>
+        [HttpPost]
+        public async Task<IActionResult> Withdraw(WithdrawRequest request)
+        {
+            if (!ModelState.IsValid)
+                return View();
+
+            WithdrawOutput output = await _withdrawUseCase.Execute(
+                request.AccountId,
+                request.Amount);
+
+            _presenter.Populate(output, this);
+
+            return _presenter.ViewModel;
+        }
+    }
+}
---
+++ src/Manga.MvcApp/UseCases/Withdraw/Presenter.cs
@@ -0,0 +1,15 @@
+﻿namespace Manga.MvcApp.UseCases.Withdraw
+{
+    using Manga.Application.UseCases.Withdraw;
+    using Microsoft.AspNetCore.Mvc;
+
+    public sealed class Presenter
+    {
+        public IActionResult ViewModel { get; private set; }
+
+        public void Populate(WithdrawOutput response, Controller controller)
+        {
+            ViewModel = controller.View();
+        }
+    }
+}
