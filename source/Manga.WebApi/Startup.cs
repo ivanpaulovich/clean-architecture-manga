cae683ae4408d7ce03263403101b5dadd4113969
Ivan Paulovich <ivan@paulovich.net>
2019-08-28 22:25:04 +0200

    Fix multiple environemt setup

--- source/Manga.WebApi/Startup.cs
+++ source/Manga.WebApi/Startup.cs
@@ -19,7 +19,7 @@ namespace Manga.WebApi
         public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
-        public void ConfigureServices(IServiceCollection services)
+        public void ConfigureDevelopmentServices(IServiceCollection services)
         {
             services.AddMvc()
                 .SetCompatibilityVersion(CompatibilityVersion.Version_2_2)
@@ -36,13 +36,39 @@ namespace Manga.WebApi
             services.AddPresentersV2();
         }
 
+        public void ConfigureProductionServices(IServiceCollection services)
+        {
+            services.AddMvc()
+                .SetCompatibilityVersion(CompatibilityVersion.Version_2_2)
+                .AddControllersAsServices();
+
+            services.AddBusinessExceptionFilter();
+
+            services.AddFeatureFlags(Configuration);
+            services.AddVersionedSwagger();
+
+            services.AddUseCases();
+            services.AddSQLServerPersistence(Configuration);
+            services.AddPresentersV1();
+            services.AddPresentersV2();
+        }
+
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(
             IApplicationBuilder app,
             IHostingEnvironment env,
             IApiVersionDescriptionProvider provider)
         {
-            app.UseDeveloperExceptionPage();
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
             app.UseVersionedSwagger(provider);
             app.UseHttpsRedirection();
             app.UseStaticFiles();
