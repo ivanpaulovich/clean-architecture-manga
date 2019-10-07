7751ec5e5dbaa0d456af238fd1869b5504c342fa
petrspelos <petrspelos@users.noreply.github.com>
2019-10-07 16:11:41 +0200

    Use Expression bodied methods in EntityFactories

--- source/Manga.Infrastructure/EntityFrameworkDataAccess/EntityFactory.cs
+++ source/Manga.Infrastructure/EntityFrameworkDataAccess/EntityFactory.cs
@@ -9,27 +9,15 @@ namespace Manga.Infrastructure.EntityFrameworkDataAccess
     public sealed class EntityFactory : IEntityFactory
     {
         public IAccount NewAccount(ICustomer customer)
-        {
-            var account = new Account(customer);
-            return account;
-        }
+            => new Account(customer);
 
         public ICredit NewCredit(IAccount account, PositiveMoney amountToDeposit, DateTime transactionDate)
-        {
-            var credit = new Credit(account, amountToDeposit, transactionDate);
-            return credit;
-        }
+            => new Credit(account, amountToDeposit, transactionDate);
 
         public ICustomer NewCustomer(SSN ssn, Name name)
-        {
-            var customer = new Customer(ssn, name);
-            return customer;
-        }
+            => new Customer(ssn, name);
 
         public IDebit NewDebit(IAccount account, PositiveMoney amountToWithdraw, DateTime transactionDate)
-        {
-            var debit = new Debit(account, amountToWithdraw, transactionDate);
-            return debit;
-        }
+            => new Debit(account, amountToWithdraw, transactionDate);
     }
 }
