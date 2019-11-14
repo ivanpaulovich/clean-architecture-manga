057c283c39f4c4d6a1b17eff87ac83bd242ebffe
Philippe Matray <phmatray@gmail.com>
2019-11-14 15:06:41 +0100

    Update MangaContext (add username and password for test user)

--- src/Infrastructure/EntityFrameworkDataAccess/MangaContext.cs
+++ src/Infrastructure/EntityFrameworkDataAccess/MangaContext.cs
@@ -77,21 +77,32 @@ namespace Infrastructure.EntityFrameworkDataAccess
                 .IsRequired();
 
             modelBuilder.Entity<Customer>().HasData(
-                new { Id = new Guid("197d0438-e04b-453d-b5de-eca05960c6ae"), Name = new Name("Test User"), SSN = new SSN("19860817-9999") }
+                new
+                {
+                    Id = new Guid("197d0438-e04b-453d-b5de-eca05960c6ae"),
+                    Name = new Name("Test User"),
+                    SSN = new SSN("19860817-9999"),
+                    Username = new Username("test"),
+                    Password = new Password("password")
+                }
             );
 
             modelBuilder.Entity<Account>().HasData(
-                new { Id = new Guid("4c510cfe-5d61-4a46-a3d9-c4313426655f"), CustomerId = new Guid("197d0438-e04b-453d-b5de-eca05960c6ae") }
+                new
+                {
+                    Id = new Guid("4c510cfe-5d61-4a46-a3d9-c4313426655f"),
+                    CustomerId = new Guid("197d0438-e04b-453d-b5de-eca05960c6ae")
+                }
             );
 
             modelBuilder.Entity<Credit>().HasData(
                 new
                 {
                     Id = new Guid("f5117315-e789-491a-b662-958c37237f9b"),
-                        AccountId = new Guid("4c510cfe-5d61-4a46-a3d9-c4313426655f"),
-                        Amount = new PositiveMoney(400),
-                        Description = "Credit",
-                        TransactionDate = DateTime.UtcNow
+                    AccountId = new Guid("4c510cfe-5d61-4a46-a3d9-c4313426655f"),
+                    Amount = new PositiveMoney(400),
+                    Description = "Credit",
+                    TransactionDate = DateTime.UtcNow
                 }
             );
 
@@ -99,10 +110,10 @@ namespace Infrastructure.EntityFrameworkDataAccess
                 new
                 {
                     Id = new Guid("3d6032df-7a3b-46e6-8706-be971e3d539f"),
-                        AccountId = new Guid("4c510cfe-5d61-4a46-a3d9-c4313426655f"),
-                        Amount = new PositiveMoney(400),
-                        Description = "Debit",
-                        TransactionDate = DateTime.UtcNow
+                    AccountId = new Guid("4c510cfe-5d61-4a46-a3d9-c4313426655f"),
+                    Amount = new PositiveMoney(400),
+                    Description = "Debit",
+                    TransactionDate = DateTime.UtcNow
                 }
             );
         }
