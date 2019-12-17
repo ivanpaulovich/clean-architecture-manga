735b28941d39a866a6d88cdb766d9fad5de7c8e3
Cesar Pereira <cesinhaugusto@gmail.com>
2019-12-17 10:19:25 -0300

    Added /metrics endpoint - Closes #137 (#139)
    * Added metrics endpoint
    * Used built in prometheus middleware. Fixes #137

--- src/WebApi/Startup.cs
+++ src/WebApi/Startup.cs
@@ -8,6 +8,7 @@ namespace WebApi
     using Microsoft.AspNetCore.Mvc.ApiExplorer;
     using Microsoft.Extensions.Configuration;
     using Microsoft.Extensions.DependencyInjection;
+    using Prometheus;
     using WebApi.DependencyInjection;
     using WebApi.DependencyInjection.FeatureFlags;
 
@@ -43,6 +44,8 @@ namespace WebApi
             IApiVersionDescriptionProvider provider)
         {
             app.UseHttpsRedirection();
+            app.UseMetricServer();
+            app.UseMangaHttpMetrics();
             app.UseDeveloperExceptionPage();
             app.UseRouting();
             app.UseVersionedSwagger(provider);
@@ -80,6 +83,8 @@ namespace WebApi
             IApiVersionDescriptionProvider provider)
         {
             app.UseHttpsRedirection();
+            app.UseMetricServer();
+            app.UseMangaHttpMetrics();
             app.UseRouting();
             app.UseVersionedSwagger(provider);
             app.UseStaticFiles();
