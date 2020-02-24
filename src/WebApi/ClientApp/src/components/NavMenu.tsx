20a2cc5da8eb18d489892227efe5a9e0ab5f290f
Ivan Paulovich <ivan@paulovich.net>
2020-02-24 22:49:51 +0100

    Review

--- src/WebApi/ClientApp/src/components/NavMenu.tsx
+++ src/WebApi/ClientApp/src/components/NavMenu.tsx
@@ -26,6 +26,9 @@ export default class NavMenu extends React.PureComponent<{}, { isOpen: boolean }
                                 <NavItem>
                                     <NavLink tag={Link} className="text-dark" to="/fetch-data">Fetch data</NavLink>
                                 </NavItem>
+                                <NavItem>
+                                    <NavLink className="text-dark" href="/login">Login with GitHub</NavLink>
+                                </NavItem>
                             </ul>
                         </Collapse>
                     </Container>
