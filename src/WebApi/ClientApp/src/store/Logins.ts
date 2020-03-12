ed7e07d5864cf6dab2bc482920a2f091d4f23147
Ivan Paulovich <ivan@paulovich.net>
2020-03-12 17:58:03 +0100

    review

--- src/WebApi/ClientApp/src/store/Logins.ts
+++ src/WebApi/ClientApp/src/store/Logins.ts
@@ -2,58 +2,34 @@ import { Action, Reducer } from 'redux';
 import { AppThunkAction } from './';
 
 export interface LoginState {
-    isLoading: boolean;
-    login: Login;
-}
-
-export interface Login {
-    userName: string;
-}
-
-interface RequestLoginAction {
-    type: 'REQUEST_LOGIN';
+    isLoggedIn: boolean;
 }
 
 interface ReceiveLoginAction {
     type: 'RECEIVE_LOGIN';
-    login: Login;
+    isLoggedIn: boolean;
 }
 
-type KnownAction = RequestLoginAction | ReceiveLoginAction;
-
 export const actionCreators = {
-    requestLogin: (): AppThunkAction<KnownAction> => (dispatch, getState) => {
-        fetch(`/api/v1/Login/GetUserInfo`)
-            .then(response => response.json() as Promise<Login>)
-            .then(login => {
-                dispatch({ type: 'RECEIVE_LOGIN', login: login });
-            });
-
-        dispatch({ type: 'REQUEST_LOGIN' });
+    requestLogin: (): AppThunkAction<ReceiveLoginAction> => (dispatch) => {
+        dispatch({type: 'RECEIVE_LOGIN', isLoggedIn: true});
     }
 };
 
-const unloadedState: LoginState = { login: { userName: "" }, isLoading: false };
-
 export const reducer: Reducer<LoginState> = (state: LoginState | undefined, incomingAction: Action): LoginState => {
     if (state === undefined) {
-        return unloadedState;
+        return { isLoggedIn: false };
     }
 
-    const action = incomingAction as KnownAction;
-    switch (action.type) {
-        case 'REQUEST_LOGIN':
-            return {
-                login: state.login,
-                isLoading: true
-            };
+    const action = incomingAction as ReceiveLoginAction;
+
+    switch(action.type) {
         case 'RECEIVE_LOGIN':
             return {
-                isLoading: false,
-                login: action.login
+                isLoggedIn: true,
             };
-            break;
-    }
 
-    return state;
+        default:
+            return state;
+    }
 };
