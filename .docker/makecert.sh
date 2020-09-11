ae444cce0adcadbea9534ffa5e5df2597256b49d
Ivan Paulovich <ivan@paulovich.net>
2020-09-11 11:54:31 +0200

    SSL

--- .docker/makecert.sh
+++ .docker/makecert.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-openssl req -x509 -newkey rsa:4096 -keyout localhost.key -out localhost.crt -days 3650 -nodes -subj "/CN=localhost" -config ssl-selfsigned.cnf
+openssl req -x509 -newkey rsa:4096 -keyout localhost.key -out localhost.crt -days 3650 -nodes -subj "/CN=wallet.local"
 openssl pkcs12 -export -out localhost.pfx -inkey localhost.key -in localhost.crt -name "Localhost selfsigned certificate" -password pass:MyCertificatePassword
 cp localhost.key ~/.aspnet/https/localhost.key
 cp localhost.crt ~/.aspnet/https/localhost.crt
