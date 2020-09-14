ec14a66d918117e8c473bfc67e0422ec3caab1dc
Ivan Paulovich <ivan.paulovich@nordax.se>
2020-09-14 22:23:38 +0200

    Review windows

--- .docker/makecert.sh
+++ .docker/makecert.sh
@@ -1,17 +1,6 @@
 #!/bin/bash
-openssl req -x509 -newkey rsa:2048 -keyout localhost.key -out localhost.crt -days 365 -nodes -config ./ssl-selfsigned.cnf
-openssl pkcs12 -export -out localhost.pfx -inkey localhost.key -in localhost.crt -name "Localhost selfsigned certificate" -password pass:MyCertificatePassword
-rm ~/.aspnet/https/localhost.key
-rm ~/.aspnet/https/localhost.crt
-rm ~/.aspnet/https/localhost.pfx
-rm ../wallet-spa/localhost.key
-rm ../wallet-spa/localhost.crt
-rm ../nginx/localhost.key
-rm ../nginx/localhost.crt
-cp localhost.key ~/.aspnet/https/localhost.key
-cp localhost.crt ~/.aspnet/https/localhost.crt
-cp localhost.pfx ~/.aspnet/https/localhost.pfx
-cp localhost.key ../wallet-spa/localhost.key
-cp localhost.crt ../wallet-spa/localhost.crt
-cp localhost.key ../nginx/localhost.key
-cp localhost.crt ../nginx/localhost.crt
+mkdir https
+openssl req -x509 -newkey rsa:2048 -keyout https/localhost.key -out https/localhost.crt -days 365 -nodes -config ./ssl-selfsigned.cnf
+openssl pkcs12 -export -out https/localhost.pfx -inkey https/localhost.key -in https/localhost.crt -name "Localhost selfsigned certificate" -password pass:MyCertificatePassword
+cp https/localhost.key ../nginx/localhost.key
+cp https/localhost.crt ../nginx/localhost.crt
---
+++ .docker/trustcert-windows.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+# https://gist.github.com/epcim/03f66dfa85ad56604c7b8e6df79614e0
+certutil -addstore -f "ROOT" https/localhost.crt
