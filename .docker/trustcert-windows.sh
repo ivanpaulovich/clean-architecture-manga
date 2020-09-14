647e69799f63943a83408010cc1173143b3d4b02
Ivan Paulovich <ivan.paulovich@nordax.se>
2020-09-14 22:26:58 +0200

    Review

--- .docker/trustcert-windows.sh
+++ .docker/trustcert-windows.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 # https://gist.github.com/epcim/03f66dfa85ad56604c7b8e6df79614e0
-certutil -addstore -f "ROOT" https/localhost.crt
+dotnet dev-certs https -ep https/localhost.pfx -p MyCertificatePassword
+dotnet dev-certs https --trust
